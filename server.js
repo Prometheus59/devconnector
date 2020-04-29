3f1ea74ca3b7f54ce4feeedd1d83af9142b5ed0a
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-04-29 00:46:14 -0400

    Authentication with jwt / passport

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
@@ -20,8 +21,11 @@ mongoose.connect(db,{useNewUrlParser: true})
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello there'));
+// Passport middleware
+app.use(passport.initialize());
 
+// Passport Config
+require('./config/passport')(passport);
 
 // Use Routes
 app.use('/api/users', users);
