a9b9c6b7e3a4db54820773e3c98e51fcb329775d
Ryan Karumanchery <ryankarumanchery@gmail.com>
2020-04-29 02:26:01 -0400

    login and registration validation implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,11 @@ const bcrypt = require("bcryptjs");
 const normalize = require("normalize-url");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-const passport = require('passport');
+const passport = require("passport");
+
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
 
 // Load user model
 const User = require("../../models/User");
@@ -19,13 +23,21 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 // @desc    Register User
 // @access  Public
 router.post("/register", async (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    // send 400 status and errors obj
+    return res.status(400).json(errors);
+  }
   const { name, email, password } = req.body;
 
   try {
     let user = await User.findOne({ email });
 
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     }
 
     // Create new user
@@ -66,12 +78,21 @@ router.post("/register", async (req, res) => {
 // @desc    Register User
 // @access  Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    // send 400 status and errors obj
+    return res.status(400).json(errors);
+  }
+
   const { email, password } = req.body;
 
   // Find user by email
   User.findOne({ email }).then((user) => {
     if (!user) {
-      return res.status(404).json({ email: "User not found" });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -92,7 +113,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password Incorrect" });
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
@@ -101,12 +123,16 @@ router.post("/login", (req, res) => {
 // @router  GET api/users/current
 // @desc    Return current user
 // @access  Private
-router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
-  res.json({
-    id: req.user.id,
-    name: req.user.name,
-    email: req.user.email
-  });
-})
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+    });
+  }
+);
 
 module.exports = router;
